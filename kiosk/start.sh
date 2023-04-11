8664516dd002f15303249df158d303febd0654b8
Chris <mail@chriswalter.org>
2023-04-11 17:21:56 +0200

    change follows

--- kiosk/start.sh
+++ kiosk/start.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# By default docker gives us 64MB of shared memory size but to display heavy
+# pages we need more.
+umount /dev/shm && mount -t tmpfs shm /dev/shm
+rm /tmp/.X0-lock &>/dev/null || true
+
 # https://github.com/balena-labs-projects/xserver
 # We are going to wait for the UNIX socket to be mounted before trying to start the application. This will prevent us from going into a crash loop before xserver is ready. See more info here: https://github.com/balenablocks/xserver#waiting-for-xserver-to-start
 while ! xset -q; do sleep 0.5; done
+
+# while [ ! -e /tmp/.X11-unix/X${DISPLAY#*:} ]; do sleep 0.5; done
+# -> remove x11-xserver-utils
 
 # Run balena base image entrypoint script. We also specified UDEV=1 in the Dockerfile. This will allow udev devices (mouse, etc) to be mounted
 /usr/bin/entry.sh echo "Running balena base image entrypoint..."
@@ -24,7 +32,7 @@ echo "Setting CPU Scaling Governor to 'performance'"
 echo 'performance' > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor 
 
 npm run startLocal
-while ! nc -z localhost 3000; do sleep 1; done
+#while ! nc -z localhost 3000; do sleep 1; done
 
 exec chromium --kiosk --no-sandbox --disable-dev-shm-usage http://localhost:3000 
 #chromium --kiosk --no-sandbox --disable-dev-shm-usage --disable-infobars --incognito http://localhost:3000 
