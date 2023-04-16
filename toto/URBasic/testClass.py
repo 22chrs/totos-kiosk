2b9e78587bf39616f2d71e32389bc623f88257d8
Chris <mail@chriswalter.org>
2023-04-16 19:03:56 +0200

    geht

--- toto/URBasic/testClass.py
+++ toto/URBasic/testClass.py
@@ -1,36 +1,29 @@
 import URBasic
+import threading
 import time
-
-# ROBOT_IP = "192.168.8.124"
-# ACCELERATION = 0.3  # Robot acceleration value
-# VELOCITY = 0.5  # Robot speed value
-
-
-
 
 class TestClass():
     def __init__(self, ip, acceleration, velocity, startposition):
-
         # Set acceleration
         self.acceleration = acceleration
         self.velocity = velocity
         self.startposition = startposition
 
-
         # Init URBasic
         self.robotModel = URBasic.robotModel.RobotModel()
         self.robot = URBasic.urScriptExt.UrScriptExt(host=ip, robotModel=self.robotModel)
 
-
-
         # Create an instance of RealTimeClient
         self.robotModel.rt_interface = ip
-       
         self.real_time_client = URBasic.realTimeClient.RealTimeClient(self.robotModel)
         
         print("Robot initialisation successful.")
 
+        # RealTimeClient
+        #realTimeClient = URBasic.realTimeClient.RealTimeClient(robotModel)
+        time.sleep(1)
         
+        print("Robot initialisation successful.")
     
 
 
