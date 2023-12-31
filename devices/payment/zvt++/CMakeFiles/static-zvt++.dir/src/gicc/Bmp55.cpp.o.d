73f16827672671969f54a50ed78210037f547bb1
Chris <mail@chriswalter.org>
2023-12-31 13:53:21 +0100

    .

--- devices/payment/zvt++/CMakeFiles/static-zvt++.dir/src/gicc/Bmp55.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/static-zvt++.dir/src/gicc/Bmp55.cpp.o: \
-  /Users/chris/Documents/Git/totos-kiosk/devices/payment/zvt++/src/gicc/Bmp55.cpp \
-  /Users/chris/Documents/Git/totos-kiosk/devices/payment/zvt++/src/gicc/Bmp55.hpp
