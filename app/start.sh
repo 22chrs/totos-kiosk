5f3879b7e0214bd795eb3a68281ea23cbcfd146d
Chris <mail@chriswalter.org>
2024-01-14 01:44:15 +0100

    .

--- app/start.sh
+++ app/start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Run balena base image entrypoint script
-#/usr/bin/entry.sh echo "Running balena base image entrypoint..."
+/usr/bin/entry.sh echo "Running balena base image entrypoint..."
 
 #start express server with nextjs files
 npm run startLocal
 
-#balena-idle
+balena-idle
