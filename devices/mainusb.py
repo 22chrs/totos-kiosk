a0bac81257226542e6c14cff764e8806633fb000
Chris <mail@chriswalter.org>
2024-08-21 00:49:18 +0200

    .

--- devices/mainusb.py
+++ devices/mainusb.py
@@ -1,30 +1,31 @@
 import asyncio
-from usbserial.usbserial import UsbSerialManager, boardserial
+from usbserial.usbserial import UsbSerialManager
 
-teensys = {"ServiceCube", "RoboCubeFront", "RoboCubeBack"}
+#teensys = {"RoboCubeFront", "ServiceCube", "RoboCubeBack"}  # Set the boards you want to test with
+teensys = {"RoboCubeFront"}  # Set the boards you want to test with
 
-# Entry point for the script
 if __name__ == '__main__':
     print("Starting event loop")
     loop = asyncio.get_event_loop()
 
-    # Create a UsbSerialManager instance to handle Teensy boards
     usb_manager = UsbSerialManager(vid=0x16C0, pid=0x0483, baudrate=115200, timeout=0.1, required_aliases=teensys)
 
-    # Define a coroutine that periodically sends messages to Teensy boards
     async def send_periodically():
         while True:
-            #print(f"board alias: {boardserial.board_info['alias']}")
-            # Send "Hello, World!" to the Teensy boards
-            #usb_manager.send_message("ServiceCube", "Hello, World!")
-
-            #usb_manager.print_object_properties()
-            #usb_manager.send_message("Motorsteuerung_B", "Hello, World!")
-
+            # Example commands
+            if "RoboCubeFront" in usb_manager.boards:
+                usb_manager.send_move_device_command("RoboCubeFront", "Schleuse", 100, 100, 100)
+            if "ServiceCube" in usb_manager.boards:
+                usb_manager.send_move_device_command("ServiceCube", "Lift_A", 100, 100, 100)
+            if "RoboCubeBack" in usb_manager.boards:
+                usb_manager.send_move_device_command("RoboCubeBack", "Rodell_C", 10, 100, 100)
+            
             await asyncio.sleep(3)
 
     async def main():
         await usb_manager.start()
-        await send_periodically()
+        asyncio.create_task(send_periodically())
+        while True:
+            await asyncio.sleep(1)
 
     loop.run_until_complete(main())
